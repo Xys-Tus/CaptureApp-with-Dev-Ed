bff46a6783646f27d34fc01be2f2b0a4a8390369
Xys-Tus <Sixtus Onyedibe>
2022-03-14 13:19:39 +0100

    5 Intro to Styled Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+// Global style
+import GlobalStyle from "./Components/GlobalStyle";
+// Import Pages
 import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
